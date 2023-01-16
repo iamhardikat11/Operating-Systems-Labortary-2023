a328066e1f333a0260a164f7251d30a06dc308c3
iamhardikat11 <iamhardikat11@gmail.com>
2023-01-16 12:37:59 +0530

    Solved Question 9

--- Assignment 1/Q9/q9.sh
+++ Assignment 1/Q9/q9.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # awk '{print $2}' < $1 | uniq -c | sort -k
-awk '{a[$2]++} END{for(s in a){print s" "a[s]}}' | sort $1
+# awk '{a[$2]++} END{for(s in a){print s" "a[s]}}'
+awk '{ print $2 }' $1 | uniq -c | sort -k 2 -t, -rn
+echo
+awk '{count[$1]++} END {for ( i in count ) {if(count[i] >= 2) {print i} if(count[i] == 1) {cnt++} } {print cnt} }' $1
+# awk '{if(count[$1]++ >= max) max = count[$1]} END {for ( i in count ) if(max == count[i]) print i, count[i] }' $1
