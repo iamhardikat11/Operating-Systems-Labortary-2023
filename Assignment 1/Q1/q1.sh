92cddfad554255df47e11d098c45187819e4a468
iamhardikat11 <iamhardikat11@gmail.com>
2023-01-16 13:12:21 +0530

    made changes to q1

--- Assignment 1/Q1/q1.sh
+++ Assignment 1/Q1/q1.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 gcd() {
-  if [ $2 -eq 0 ] 
-   then 
-    return $1
-  else
-    return $(gcd $2 `expr $1 % $2`)
-  fi
+if [ $2 -eq 0 ]
+then
+return $1
+else
+return $(gcd $2 `expr $1 % $2`)
+fi
 }
 ans=1
-while read line; do
-    let "num=$num | rev"
-    gcd $ans $num
-    let "ans=(ans*num)/$?"
-done < $1
-echo "$ans"
+for num in $(rev $1); do
+gcd $ans $num
+let "ans=(ans*num)/$?"
+done
+echo $ans
