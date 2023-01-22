937a2cd20f1534f2b08bcdfce594ac172e905298
iamhardikat11 <iamhardikat11@gmail.com>
2023-01-22 17:31:20 +0530

    Updated Question 6

--- Assignment 1/Q6/Assgn1_6_11.sh
+++ Assignment 1/Q6/Assgn1_6_11.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-limit=1000000
 declare -a spf
-Sieve_of_Eratosthene() {
-    for((i=1; i<=limit; i++))
+
+Sieve_of_Eratosthenes() {
+    for((i=2; i<=1000000; i++))
     do
         spf[i]=$i
     done
-    for((i=4; i<=limit; i=i+2))
+    for((i=4; i<=1000000; i=i+2))
     do
         spf[i]=2
     done
-    for((i=3; i*i<=limit; i++))
+    for((i=3; i*i<=1000000; i=i+2))
     do 
         if [[ ${spf[i]} -eq $i ]]; then
-            for((j=i*i; j<=limit; j+=i))
+            for((j=i*i; j<=1000000; j+=i))
             do
-                if [ ${sp[j]} -eq $j ]; then
-                    spf[j]=$i
-                fi
+            if [ ${spf[j]} -eq $j ]; then
+                spf[j]=$i
+            fi
             done   
         fi 
     done
 }
-Sieve_of_Eratosthene
+
+Sieve_of_Eratosthenes
 while read -r x
 do
     while [[ $x -ne 1 ]]
@@ -32,4 +33,5 @@ do
     done
     echo
 done < $1
+
 
