29764ea92b3de3682d1198c6209333d555f7b7b4
YATHARTH SAMEER <ytharthsmr@gmail.com>
2023-01-24 12:22:53 +0530

    some formatting

--- Assignment 1/Q5/Assgn1_5_11.sh
+++ Assignment 1/Q5/Assgn1_5_11.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
 folder=$1
+
+if [ -z "$folder" ]; then
+    echo "Usage: $0 <folder>"
+    exit 1
+fi
+
+if [ ! -d "$folder" ]; then
+    echo "Error: $folder is not a directory"
+    exit 1
+fi
+
 for file in $(find $folder -name "*.py"); do
     echo "-----------------"
-    echo "-----------------"
-    echo "Name of the file : $file"
-        echo "-----------------"
-    echo "-----------------"
-    awk '
-    /^[^#]/ {
-        if(in_multiline_comment) {
-            printf("Line %d-%d: %s\n", start_line, NR, multiline_comment);
-            in_multiline_comment = 0;
-            multiline_comment = "";
-        }
-    }
-    /^#/ {
-        if(in_multiline_comment) {
-            multiline_comment = multiline_comment $0 "\n";
-        } else {
-            printf("Line %d: %s\n", NR, $0);
-        }
-    }
-   
-    ' $file
+    echo $file
     echo "-----------------"
     echo "Multiline comments in the file"
     echo "-----------------"
@@ -34,20 +25,9 @@ for file in $(find $folder -name "*.py"); do
         if [ $flag -eq 0 ]; then
           flag=1
           output="$output $word"
-           while IFS= read -r line; do
-        n=$((n+1))
-        if [[ $line == *"$word"* ]]; then
-              echo "line $n:"
-              break
-          fi
-    
-          done < "$file"
         else
           flag=0
           output="$output $word"
-          n=0
-        newline_count=0
-     
           echo "$output"
           output=""
         fi
@@ -64,5 +44,10 @@ for file in $(find $folder -name "*.py"); do
       echo "$output "
     fi
     echo "-----------------"
-   
+    echo "-----------------"
+    echo "Single line comments in the file"
+    echo "-----------------"
+    grep -E -n "^[[:space:]]*#" $file
+    echo "-----------------"
+    echo "-----------------"
 done
