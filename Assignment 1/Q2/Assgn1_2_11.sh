cf5a4beca7f8c15b6cf2dd2d89994e44126320b7
iamhardikat11 <iamhardikat11@gmail.com>
2023-01-25 03:16:01 +0530

    Reduced Code for Q1 and Q2

--- Assignment 1/Q2/Assgn1_2_11.sh
+++ Assignment 1/Q2/Assgn1_2_11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while read username;
 do
-    if [[ $username =~ ^.{5,20}$ ]] && [[ $username =~ ^[a-zA-Z][a-zA-Z0-9]*[0-9][a-zA-Z0-9]*$ ]] <<< "$username" && ! grep -iqFf "fruits.txt" <<< "$username"
+    if [[ $username =~ ^.{5,20}$ ]] && [[ $username =~ ^[a-zA-Z][a-zA-Z0-9]*[0-9][a-zA-Z0-9]*$ ]] && ! grep -iqFf "fruits.txt" <<< "$username"
     then
         echo "YES" >> validation_results.txt
     else
