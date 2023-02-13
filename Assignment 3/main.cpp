6ce8c1ece373787f5c179d3020586242a805c0f4
iamhardikat11 <iamhardikat11@gmail.com>
2023-02-14 02:46:19 +0530

    Completed IPC Shared Memory

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -1,60 +1,52 @@
+// No. of Nodes:- 4039
 #include <bits/stdc++.h>
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <fstream>
-#include <string>
+#include <sys/shm.h>
 using namespace std;
 
-// Represent a vertex in the graph
-class Vertex {
-public:
-    int id;
-    vector<int> neighbors;
-    Vertex(int id) 
-    {
-        this->id = id;
+const int MAX_NODES = 4039;
+
+int main()
+{
+    char* myseg;
+    key_t key;
+    int shmid;
+
+    key = 300; // some unique_id
+
+    shmid = shmget(key, 250, IPC_CREAT | 0666);
+    // start of IPC Segment
+    myseg = (char *)shmat(shmid, NULL, 0);
+
+    vector<vector<int>> graph(MAX_NODES); // Creating the graph using vector of vectors
+
+    // Opening the file
+    ifstream input("facebook_combined.txt");
+    if (!input) {
+        cout << "Unable to open the file." << endl;
+        return 1;
     }
-};
 
-// Represent a graph
-class Graph {
-public:
-    vector<Vertex> vertices;
-    unordered_map<int, int> idToIndex;
+    int a, b;
+    int cnt = 0;
+    while (input >> a >> b) {
+        graph[a].push_back(b); // Adding the edge from node 'a' to node 'b'
+        graph[b].push_back(a); // Adding the edge from node 'b' to node 'a'
+        cnt++;
+    }
+    // Closing the file
+    input.close();
 
-    Graph(int numVertices) {
-        vertices.resize(numVertices);
-        for (int i = 0; i < numVertices; i++) {
-            vertices[i] = Vertex(i);
-            idToIndex[i] = i;
+    // Printing the graph to check if it is properly stored
+    for (int i = 0; i < graph.size(); i++) {
+        cout << i << ": ";
+        for (int j = 0; j < graph[i].size(); j++) {
+            cout << graph[i][j] << " ";
         }
+        cout << endl;
     }
+    cout << "Edges:- " << cnt << endl;
+    shmdt(myseg);
+    // end of IPC Segment
+    shmctl(shmid, IPC_RMID, NULL);
 
-    void addEdge(int source, int dest) {
-        vertices[idToIndex[source]].neighbors.push_back(dest);
-        // vertices[idToIndex[source]].neighbors.push_back(dest);
-    }
-};
-
-int main() {
-    Graph g = new Graph(4039);
-    ifstream file("facebook_combined.txt");
-    string line;
-    while (getline(file, line)) {
-        int source, dest;
-        sscanf(line.c_str(), "%d %d", &source, &dest);
-        // g.addEdge(source, dest);
-    }
-
-    // // Printing the graph
-    // for (int i = 0; i < g.vertices.size(); i++) {
-    //     cout << "Vertex " << i << " is connected to: ";
-    //     for (int j = 0; j < g.vertices[i].neighbors.size(); j++) {
-    //         cout << g.vertices[i].neighbors[j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    return 0;
 }
